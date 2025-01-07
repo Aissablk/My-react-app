352bcfeb1ea11284fe435516cbd4c1bb50535718
AissaBilek <aissa.bilek@infra-automation.com>
2025-01-07 20:09:26 +0100

    using ? conditionally rendering UI .

---
+++ src/Conditions.jsx
@@ -0,0 +1,11 @@
+function Lion(){
+    const Animals=['Lion','Vache','dog','mouton'];
+    return(
+    <div>
+    {Animals.map((num, index) =>  num.startsWith('L') ? <div key={index}> {num}</div > : Null )}
+    
+    </div> 
+    
+    )
+ }
+export default Lion; 
--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,11 @@ import HandleList from './Rendering_elements.jsx'
 import OneElement from './Rendering_composants.jsx'
 import ListItem from './Rendering_composants.jsx'
 import Affiche from './Rendering_composants.jsx'
+import Lion from './Conditions.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Affiche />
+   <Lion /> 
   </StrictMode>, 
-)
+) 
