9a7d00d8f8ceee2bdfc077dd9696ed5e4ace8bb8
AissaBilek <aissa.bilek@infra-automation.com>
2025-01-07 17:05:45 +0100

    rendering composants using JSX.

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,13 @@ import Convert from './Convert.jsx'
 import TodoList from './Date.jsx'
 import FormatDate from './Date.jsx'
 import HandleList from './Rendering_elements.jsx'
+import OneElement from './Rendering_composants.jsx'
+import ListItem from './Rendering_composants.jsx'
+import Affiche from './Rendering_composants.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HandleList />
+   <Affiche />
   </StrictMode>, 
 )
