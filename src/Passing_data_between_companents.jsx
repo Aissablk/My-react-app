8e8a88878f6c08a1db5cb26d47e2f4d885abec61
AissaBilek <aissa.bilek@infra-automation.com>
2025-01-11 12:14:44 +0100

    using props to  display 2 differents buttons

--- src/Passing_data_between_companents.jsx
+++ src/Passing_data_between_companents.jsx
@@ -1,7 +1,13 @@
-function Button(){
+function Button(props){
+    const buttonStyle={
+        color : props.color,
+        fontSize : props.fontSize +"px" 
+
+
+    };
     return <div>
-        <button>
-            Click on me
+        <button style={buttonStyle}>
+            {props.text}
         </button>
     </div>
 }
@@ -9,8 +15,8 @@ function Button(){
 export default function ButtonList(){
     return(
     <div>
-        <Button />
-        <Button />
+        <Button color="red" fontSize={12} text="dont click now" />
+        <Button   color="green" fontSize={12} text="click now"/> 
     </div>
     )
-}
+} 
