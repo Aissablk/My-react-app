f9fce247ad7c1ecf03cf4ab194b2941c53c76588
AissaBilek <aissa.bilek@infra-automation.com>
2025-01-11 11:37:16 +0100

    using button in different components

---
+++ src/Passing_data_between_companents.jsx
@@ -0,0 +1,14 @@
+function Button(){
+    <div>
+        <button>
+            Click on me
+        </button>
+    </div>
+}
+
+export default function(){
+    <div>
+        <Button />
+        <Button />
+    </div>
+}
